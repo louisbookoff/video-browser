95dfddfb12747b34540c6735547eb25771f5f9e1
louisbookoff <lsbookof@gmail.com>
2019-04-01 16:29:07 -0400

    new commit for video-browser

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<!-- root component -->
+<template>
+  <div>
+    Hi There!
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
---
+++ src/main.js
@@ -0,0 +1,8 @@
+import Vue from 'vue' // webpack automaticxally looks in the node_modules directory
+import App from './App'
+
+new Vue ({
+  // el: '#app', same thing as $mount below
+  render: h => h(App)
+}).$mount('app')
+// h stands for createElement - abbreviation
